339b7a7bbb63b9f6fac38048c37f5dc9c715d8a2
Jérémie P <jpourageaux@hotmail.fr>
2025-02-18 17:03:10 +0100

    create the tablabble component

---
+++ firstProject/src/components/TablabbleTextArea.vue
@@ -0,0 +1,22 @@
+<template>
+      <textarea @keydown.tab.prevent=onTabPress
+                style="width:100%; height:300px "
+      >hi there
+      </textarea>
+</template>
+
+<script setup lang="ts">
+function onTabPress(e) {
+  let textArea = e.target;
+
+  let val = textArea.value,
+    start = textArea.selectionStart,
+    end = textArea.selectionEnd;
+
+  textArea.value = val.substring(0, start) + "\t" + val.substring(end);
+
+  textArea.selectionStart = textArea.selectionEnd = start + 1;
+
+}
+
+</script>
--- firstProject/src/views/HomeView.vue
+++ firstProject/src/views/HomeView.vue
@@ -1,28 +1,14 @@
 <script setup lang="ts">
 import {useLocalStorage} from "@/composables/useLocalStorage.ts";
+import TablabbleTextArea from "@/components/TablabbleTextArea.vue";
 
 let food = useLocalStorage('food', 'auto')
-
-function onTabPress(e) {
-  let textArea = e.target;
-
-  let val = textArea.value,
-    start = textArea.selectionStart,
-    end = textArea.selectionEnd;
-
-  textArea.value = val.substring(0, start) + "\t" + val.substring(end);
-
-  textArea.selectionStart = textArea.selectionEnd = start + 1;
-
-}
-
 </script>
 
 <template>
   <main>
     <form>
-      <textarea @keydown.tab.prevent=onTabPress
-                style="width:100%; height:300px ">hi there</textarea>
+      <TablabbleTextArea/>
     </form>
     <p>
       what is your favourite food : <input v-model="food">
