5ec65311dcbc090ed0b47fb0440a448189e692e0
Jérémie P <jpourageaux@hotmail.fr>
2025-02-18 16:37:35 +0100

    add function logic"

--- firstProject/src/views/HomeView.vue
+++ firstProject/src/views/HomeView.vue
@@ -3,23 +3,27 @@ import {useLocalStorage} from "@/composables/useLocalStorage.ts";
 
 let food = useLocalStorage('food', 'auto')
 
-// let food = ref(localStorage.getItem('food'));
-//
-// watch(food, (val) => {
-//   if (val) {
-//     write('food', val)
-//   }
-// })
-//
-// function write(key: string, value: string) {
-//   localStorage.setItem(key, value)
-// }
+function onTabPress(e) {
+  let textArea = e.target;
+
+  let val = textArea.value,
+    start = textArea.selectionStart,
+    end = textArea.selectionEnd;
+
+  textArea.value = val.substring(0, start) + "\t" + val.substring(end);
+
+  textArea.selectionStart = textArea.selectionEnd = start + 1;
+
+}
 
 </script>
 
 <template>
   <main>
-
+    <form>
+      <textarea @keydown.tab.prevent=onTabPress
+                style="width:100%; height:300px ">hi there</textarea>
+    </form>
     <p>
       what is your favourite food : <input v-model="food">
     </p>
