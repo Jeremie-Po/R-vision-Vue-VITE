0da10b7d8554778abcd7a50c3231aae899b90cda
Jérémie P <jpourageaux@hotmail.fr>
2025-02-19 12:26:32 +0100

    exemple of injections

--- firstProject/src/views/HomeView.vue
+++ firstProject/src/views/HomeView.vue
@@ -1,21 +1,32 @@
-<script setup lang="ts">
-import {useLocalStorage} from "@/composables/useLocalStorage.ts";
-import TablabbleTextArea from "@/components/TablabbleTextArea.vue";
-import {ref} from "vue";
-
-let food = useLocalStorage('food', 'auto');
-let comment = ref('hi there');
-
-</script>
-
 <template>
   <main>
     <form>
-      <TablabbleTextArea v-model="comment" style="width:100%; height:300px "
-      />
+      <TablabbleTextArea v-model="comment" style="width:100%; height:300px "/>
     </form>
     <p>
       what is your favourite food : <input v-model="food">
     </p>
+    <Footer/>
   </main>
 </template>
+
+<script setup lang="ts">
+import {useLocalStorage} from "@/composables/useLocalStorage.ts";
+import TablabbleTextArea from "@/components/TablabbleTextArea.vue";
+import {provide, ref} from "vue";
+import Footer from "@/components/footer.vue";
+
+
+let food = useLocalStorage('food', 'auto');
+let comment = ref('hi there');
+let tab = {
+  'q1': 'hello',
+  'q2': 'there',
+}
+provide('key', 'hakounamatata');
+provide('comment', {
+  comment,
+  changeComment: () => comment.value = 'hello',
+});
+provide('tab', tab);
+</script>
