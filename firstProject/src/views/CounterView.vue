96c969771837001531a164261cd09b8f46f1ca99
Jérémie P <jpourageaux@hotmail.fr>
2025-02-20 15:38:51 +0100

    keep the counter count from file to all the pages until we dont refresh page

---
+++ firstProject/src/views/CounterView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    {{ counter.count }}
+    <button @click="counter.increment">Increment</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {counter} from '@/stores/counterStore.ts'
+</script>
--- firstProject/src/views/HomeView.vue
+++ firstProject/src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
-  <main>
-    <form>
-      <TablabbleTextArea v-model="comment" style="width:100%; height:300px "/>
-    </form>
-    <p>
-      what is your favourite food : <input v-model="food">
-    </p>
-    <Footer/>
-    <p>
-      <h2>{{ state.name }}</h2>
-      <button @click="state.name= 'Change title with this message'">Change title with this message
-      </button>
-    </p>
-  </main>
+  <form>
+    <TablabbleTextArea v-model="comment" style="width:100%; height:300px "/>
+  </form>
+  <p>
+    what is your favourite food : <input v-model="food">
+  </p>
+  <Footer/>
+  <p>
+    <h2>{{ state.name }}</h2>
+    <button @click="state.name= 'Change title with this message'">Change title with this message
+    </button>
+  </p>
+  <p>
+    the counter is at Number : {{ counter.count }}
+  </p>
 </template>
 
 <script setup lang="ts">
@@ -21,7 +22,7 @@ import TablabbleTextArea from "@/components/TablabbleTextArea.vue";
 import {provide, ref} from "vue";
 import Footer from "@/components/footer.vue";
 import {state} from "@/stores/quizzStore.ts";
-
+import {counter} from "@/stores/counterStore.ts"
 
 let food = useLocalStorage('food', 'auto');
 let comment = ref('hi there !!');
